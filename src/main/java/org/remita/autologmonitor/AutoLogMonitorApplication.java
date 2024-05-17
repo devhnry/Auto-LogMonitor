6f62f70d408a19cfc35151efb66decf0418daab3
devhnry <devwhenry@gmail.com>
2024-05-17 11:00:27 +0100

    Initial Commit

---
+++ src/main/java/org/remita/autologmonitor/AutoLogMonitorApplication.java
@@ -0,0 +1,13 @@
+package org.remita.autologmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoLogMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoLogMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/remita/autologmonitor/AutoLogMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.remita.autologmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoLogMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
