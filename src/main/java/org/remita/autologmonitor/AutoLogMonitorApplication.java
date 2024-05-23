54e2641939e9353faf95d2679536062adc1afa63
devhnry <devwhenry@gmail.com>
2024-05-23 16:46:32 +0100

    Added Cron Job to Deletion Service and fixed catching of empty line

--- src/main/java/org/remita/autologmonitor/AutoLogMonitorApplication.java
+++ src/main/java/org/remita/autologmonitor/AutoLogMonitorApplication.java
@@ -2,8 +2,10 @@ package org.remita.autologmonitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class AutoLogMonitorApplication {
 
     public static void main(String[] args) {
