2c4845472a9164f0fef85c10c802ce0713d876df
devhnry <devwhenry@gmail.com>
2024-06-24 16:51:14 +0100

    feat: Implemented JWTSecurityFilter to use JwtService and TokenRepo for token generation

--- src/main/java/org/remita/autologmonitor/config/JWTSecurityFilter.java
+++ src/main/java/org/remita/autologmonitor/config/JWTSecurityFilter.java
@@ -5,20 +5,32 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.remita.autologmonitor.entity.Token;
+import org.remita.autologmonitor.repository.TokenRepository;
+import org.remita.autologmonitor.service.JWTService;
 import org.remita.autologmonitor.service.UserDetailService;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.function.Function;
+
 @Component
 @RequiredArgsConstructor
 public class JWTSecurityFilter extends OncePerRequestFilter {
 
     private final UserDetailService userDetailService;
+    private final JWTService jwtService;
+    private final TokenRepository tokenRepository;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
+            throws ServletException, IOException{
         final String authHeader = request.getHeader("Authorization");
         final String jwtToken;
         final String userEmail;
@@ -27,6 +39,28 @@ public class JWTSecurityFilter extends OncePerRequestFilter {
             return;
         }
         jwtToken = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwtToken);
+
+        if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            UserDetails userDetails = userDetailService.loadUserByUsername(userEmail);
+
+            Function<Token, Boolean> validateToken = t -> !t.getExpired().equals(true) && !t.getRevoked().equals(true);
+
+            var isTokenValid = tokenRepository.findByToken(jwtToken).map(
+                    validateToken
+            ).orElse(false);
+
+
+            if(jwtService.isTokenValid(jwtToken, userDetails) && isTokenValid){
+                SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+                UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities()
+                );
+                token.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                securityContext.setAuthentication(token);
+                SecurityContextHolder.setContext(securityContext);
+            }
+        }
         filterChain.doFilter(request, response);
     }
 }
--- src/main/java/org/remita/autologmonitor/service/JWTService.java
+++ src/main/java/org/remita/autologmonitor/service/JWTService.java
@@ -1,2 +1,65 @@
-package org.remita.autologmonitor.service;public class JWTService {
+package org.remita.autologmonitor.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.function.Function;
+
+@Service
+public class JWTService {
+
+    private SecretKey key;
+    private byte[] keyByte;
+    private static final long EXPIRATION_TIME = 86400000; //24hrs
+
+    public JWTService() {
+        String secretString = "HFUKNSF09839IKSFV9348J39GHIUEWFJR9EF089WUJ4FNR9HG738UJW4ONEMSCO";
+        keyByte = Base64.getDecoder().decode(secretString.getBytes(StandardCharsets.UTF_8));
+        this.key = new SecretKeySpec(keyByte, "HmacSHA256");
+    }
+
+    public String generateToken(UserDetails userDetails){
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.valueOf("HmacSHA256"), keyByte)
+                .compact();
+    }
+
+    public String generateRefreshToken(HashMap<String, Object> claims, UserDetails userDetails){
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.valueOf("HmacSHA256"), keyByte)
+                .compact();
+    }
+
+    public <T> T extractClaims(String token, Function<Claims, T> claimsTFunction){
+        return claimsTFunction.apply(Jwts.parser().setSigningKey(key).parseClaimsJwt(token).getBody());
+    }
+
+    public String extractUsername(String token){
+        return extractClaims(token, Claims::getSubject);
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    public boolean isTokenExpired(String token) {
+        return extractClaims(token, Claims::getExpiration).before(new Date());
+    }
 }
